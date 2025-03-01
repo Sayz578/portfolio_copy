d5fd6cdfc330fea66805ba08ef718280323efe16
Sayz578 <kamaevyaroslav8@gmail.com>
2025-03-01 21:15:23 +0200

    commit
    add about>skill

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const nav = document.querySelector(".nav")
 burger.addEventListener("click", function () {
     burger.classList.toggle("active")
     nav.classList.toggle("active")
+    document.body.classList.toggle("hedden")
 })
 
 $(document).ready(function () {
@@ -14,7 +15,31 @@ $(document).ready(function () {
         autoplay: true,
         dots:true,
         prevArrow: "<button type='button' class='slick-prev slider-left'> <img src = './images/Arrow-left.svg'alt = 'arow' /></button>",
-        nextArrow: "<button type='button' class='slick-next slider-right'> <img src = './images/Arrow-right.svg' alt = 'arow' /></button>"
+        nextArrow: "<button type='button' class='slick-next slider-right'> <img src = './images/Arrow-right.svg' alt = 'arow' /></button>",
+        responsive: [
+            {
+                breakpoint: 1161,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 801,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            }
+        ]
     });
 
     $('.testimonial-slider').slick({
@@ -23,7 +48,16 @@ $(document).ready(function () {
         slidesToScroll: 1,
         dots:true,
         prevArrow: "<button type='button' class='slick-prev slider-left'> <img src = './images/Arrow-left.svg'alt = 'arow' /></button>",
-        nextArrow: "<button type='button' class='slick-next slider-right'> <img src = './images/Arrow-right.svg' alt = 'arow' /></button>"
+        nextArrow: "<button type='button' class='slick-next slider-right'> <img src = './images/Arrow-right.svg' alt = 'arow' /></button>",
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    arrows: false,
+                    autoplay: true,
+                }
+            }
+        ]
     })
     let slider = $('.testimonial-slider')
     let totalslider = slider.children().length
@@ -39,3 +73,49 @@ $(document).ready(function () {
 const year = new Date().getFullYear()
 document.querySelector(".yaer").textContent = year
 
+// header fixed
+
+const headerTop = document.querySelector(".header-top")
+const btnUp = document.querySelector(".arrow-up")
+
+window.addEventListener("scroll", function(){
+    if(window.scrollY > 1){
+        headerTop.classList.add("active")
+    } else {
+        headerTop.classList.remove("active")
+    }
+})
+
+const links = document.querySelectorAll(".link")
+// console.log(links);
+for (let i = 0; i < links.length; i++) {
+    const element = links[i];
+    // console.log(element);
+    element.addEventListener("click", function(){
+        for (let i = 0; i < links.length; i++) {
+            const link = links[i];
+            link.classList.remove("active")
+        }
+        
+        element.classList.add("active")
+    })
+    
+}
+
+// button up
+window.addEventListener("scroll", function(){
+    if(this.window.scrollY > 300){
+        btnUp.classList.add("active")
+    } else {
+        btnUp.classList.remove("active")
+    }
+
+})
+
+
+btnUp.addEventListener("click", function () {
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+})
